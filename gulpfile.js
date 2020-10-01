7e9fbb75bb130c26c17c90f37bcb85202156bc63
kavindu yasintha silva <10707373@students.plymouth.ac.uk>
2020-10-01 12:33:16 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,19 @@ const minify = require('gulp-clean-css')
 const connect = require('gulp-connect')
 const autoprefixer = require('gulp-autoprefixer')
 
-const root = yargs.argv.root || '.'
+const root = yargs.argv.root || '.' dhkuhkp;
+
+dflvmk;lvk;f;
+fmlvmf;ll;
+,flvdf;v;
+,l;fl';f'
+,f';fl
+'
+;fl;gl;l]eopp[43
+              4l;k[
+              l;k3p9490
+              l;jiodno
+              
 const port = yargs.argv.port || 8000
 
 const banner = `/*!
